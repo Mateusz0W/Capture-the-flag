9f78f7eba56708ea65c3ccf3958d429ef001cddb
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-02-09 19:25:32 +0100

    add players to simulation

--- simulation.py
+++ simulation.py
@@ -1,5 +1,12 @@
 from map import Map
+from player import Player
+from config import GameConfig
 
 class Simulation:
     def __init__(self):
         self.map=Map()
+        self.players=[
+            [Player('Blue',self.map)for i in range(GameConfig.players_in_team)]+
+            [Player('Red',self.map)for i in range(GameConfig.players_in_team)]
+        ]
+        
