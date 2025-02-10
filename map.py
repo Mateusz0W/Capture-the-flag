27283a569dfcb08db1178074415ac49d2b070366
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-02-10 12:43:03 +0100

    add update method and initial grid state

--- map.py
+++ map.py
@@ -5,6 +5,7 @@ class Map:
     def __init__(self):
         self.flags_positions=[]
         self.grid=self.generate_map()
+        self.init_grid_state=self.grid.copy()
 
     def generate_map(self):
         grid = np.zeros((MapConfig.grid_y, MapConfig.grid_x), dtype=int)
@@ -18,22 +19,26 @@ class Map:
                     if  center_x-MapConfig.base_size//2 < j < center_x+MapConfig.base_size//2:
                         #flag
                         if i==3+base_cenetr and j==center_x:
-                            grid[i][j]=2
                             self.flags_positions.append((i,j))
                         #base field
-                        else:
-                            grid[i][j]=1
+                        grid[i][j]=1
                 elif MapConfig.grid_y-3-MapConfig.base_size < i < MapConfig.grid_y-3:
                     if  center_x-MapConfig.base_size//2 < j < center_x+MapConfig.base_size//2:
                         #flag
                         if i==MapConfig.grid_y-3-base_cenetr and j==center_x:
-                            grid[i][j]=-2
                             self.flags_positions.append((i,j))
                         #base field
-                        else:
-                            grid[i][j]=-1
+                        grid[i][j]=-1
 
         
         return grid
+    
+    def update(self,players):
+        self.grid=self.init_grid_state.copy()
+        for player in players:
+            if player.team =='Blue':
+                self.grid[player.x][player.y]=3
+            else:
+                self.grid[player.x][player.y]=-3
                 
         
