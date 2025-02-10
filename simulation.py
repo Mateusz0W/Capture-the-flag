c0ead965f5eab2d29b3bf1482330bbb04d92290d
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-02-10 12:44:21 +0100

    add next_step and run methods

--- simulation.py
+++ simulation.py
@@ -1,3 +1,4 @@
+import random
 from map import Map
 from player import Player
 from config import GameConfig
@@ -6,7 +7,16 @@ class Simulation:
     def __init__(self):
         self.map=Map()
         self.players=[
-            [Player('Blue',self.map)for i in range(GameConfig.players_in_team)]+
-            [Player('Red',self.map)for i in range(GameConfig.players_in_team)]
-        ]
-        
+            Player(color,self.map)
+            for color in ('Blue','Red')
+            for _ in range(GameConfig.players_in_team)
+        ]     
+    
+    def next_step(self):
+        movement=['up','down','left','right','None']
+        for player in self.players:
+            player.move(random.choice(movement),self.map)
+
+    def run(self):
+        self.next_step()
+        self.map.update(self.players)
