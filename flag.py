59e54e436f55569221148b40cabf145412b0f416
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-02-12 20:07:08 +0100

    detecting opponent flag in base

--- flag.py
+++ flag.py
@@ -1,11 +1,39 @@
+from config import MapConfig
+
 class Flag:
     def __init__(self,position,team):
         self.x,self.y=position
         self.team=team
-        self.player_with_flag=None
         self.captured=False
 
-    def update(self):
-        if self.player_with_flag is not None:
-            self.x=self.player_with_flag.x
-            self.y=self.player_with_flag.y
+    def update(self,player):
+        if player is not None:
+            self.x=player.x
+            self.y=player.y
+
+    def in_opponent_base(self):
+        center_x = MapConfig.grid_x//2
+        start_x = center_x-MapConfig.base_size // 2
+        end_x = center_x+MapConfig.base_size // 2
+
+        blue_base_start_y = MapConfig.y_offset
+        blue_base_end_y = MapConfig.y_offset+MapConfig.base_size
+        red_base_start_y = MapConfig.grid_y - MapConfig.y_offset-MapConfig.base_size
+        red_base_end_y = MapConfig.grid_y - MapConfig.y_offset
+
+        if self.team == 'Red':
+            if start_x < self.x < end_x and blue_base_start_y < self.y < blue_base_end_y:
+                return True
+            else:
+                return False
+        elif self.team == 'Blue':
+            if start_x < self.x < end_x and red_base_start_y < self.y < red_base_end_y:
+                return True
+            else:
+                return False
+        
+
+    def reset(self,position):
+        self.x,self.y=position
+        self.captured=False
+        
--- player.py
+++ player.py
@@ -6,11 +6,10 @@ class Player:
         self.team=team
         self.x,self.y=self.set_starting_position(map)
         self.has_a_flag=False
+        self.flag=None
 
     def set_starting_position(self,map):
-        index=0  # blue team
-        if self.team == 'Red':
-            index=1
+        index = 0 if self.team == 'Blue' else 1
 
         start_x=map.flags_positions[index][0]-MapConfig.base_size//2
         end_x=map.flags_positions[index][0]+MapConfig.base_size//2
@@ -21,42 +20,46 @@ class Player:
             x=random.randint(start_x,end_x)
             y=random.randint(start_y,end_y)
 
-            if map.grid[x][y]==1 and self.team=='Blue':
-                map.grid[x][y]=3
+            if map.grid[y][x]==1 and self.team=='Blue':
+                map.grid[y][x]=3
                 return x,y
-            elif  map.grid[x][y]==-1 and self.team=='Red':
-                map.grid[x][y]=-3
+            elif  map.grid[y][x]==-1 and self.team=='Red':
+                map.grid[y][x]=-3
                 return x,y
             
     def move(self,direction,map):
         occupied_fields=[-3,3,4,6,-6]
         if direction == 'up':
-            if self.y-1>=0 and map.grid[self.x][self.y-1] not in occupied_fields:
+            if self.y-1>=0 and map.grid[self.y-1][self.x] not in occupied_fields:
                 self.y-=1
         elif direction =='down':
-            if self.y+1<MapConfig.grid_y and map.grid[self.x][self.y+1] not in occupied_fields:
+            if self.y+1<MapConfig.grid_y and map.grid[self.y+1][self.x] not in occupied_fields:
                 self.y+=1
         elif direction == 'left':
-            if self.x-1>=0 and map.grid[self.x-1][self.y] not in occupied_fields:
+            if self.x-1>=0 and map.grid[self.y][self.x-1] not in occupied_fields:
                 self.x-=1
         elif direction == 'right':
-            if self.x+1<MapConfig.grid_x and map.grid[self.x+1][self.y] not in occupied_fields:
+            if self.x+1<MapConfig.grid_x and map.grid[self.y][self.x+1] not in occupied_fields:
                 self.x+=1
+            
+        if self.has_a_flag and self.flag is not None:
+            self.flag.update(self)
+
 
     def build(self,direction,map):
         occupied_fields=[-2,2-3,3,4,6,-6]
         if direction == 'up':
-            if self.y-1>=0 and map.grid[self.x][self.y-1] not in occupied_fields:
-                map.init_grid_state[self.x][self.y-1]=4
+            if self.y-1>=0 and map.grid[self.y-1][self.x] not in occupied_fields:
+                map.init_grid_state[self.y-1][self.x]=4
         elif direction =='down':
-            if self.y+1<MapConfig.grid_y and map.grid[self.x][self.y+1] not in occupied_fields:
-                map.init_grid_state[self.x][self.y+1]=4
+            if self.y+1<MapConfig.grid_y and map.grid[self.y+1][self.x] not in occupied_fields:
+                map.init_grid_state[self.y+1][self.x]=4
         elif direction == 'left':
-            if self.x-1>=0 and map.grid[self.x-1][self.y] not in occupied_fields:
-                map.init_grid_state[self.x-1][self.y]=4
+            if self.x-1>=0 and map.grid[self.y][self.x-1] not in occupied_fields:
+                map.init_grid_state[self.y][self.x-1]=4
         elif direction == 'right':
-            if self.x+1<MapConfig.grid_x and map.grid[self.x+1][self.y] not in occupied_fields:
-                map.init_grid_state[self.x+1][self.y]=4
+            if self.x+1<MapConfig.grid_x and map.grid[self.y][self.x+1] not in occupied_fields:
+                map.init_grid_state[self.y][self.x+1]=4
 
     def captured_the_flag(self,flags):
         if self.has_a_flag:
@@ -64,13 +67,16 @@ class Player:
         
         for flag in flags:
             if flag.captured:
-                return
+                continue
             
             if self.team != flag.team and self.x == flag.x and self.y == flag.y:
                 self.has_a_flag=True
-                flag.player_with_flag=self
+                self.flag=flag
                 flag.captured=True
-                return
+                break
             
+    def reset(self,map):
+        self.x , self.y =   self.set_starting_position(map)
+        self.has_a_flag=False
 
 
