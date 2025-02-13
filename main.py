03d6afe19b74ab2668eccb256fb3b0d21d48edd6
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-02-13 20:01:12 +0100

    reorganisation of the project structure

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from renderer import Renderer
-from simulation import Simulation
+import gym
+import gym_environment
 
 if __name__=="__main__":
-    simulation=Simulation()
-    renderer=Renderer(simulation)
-    while True:
-        renderer.render()
+    env=gym.make('gym_environment/Capture-the-flag-v0',render_mode='human')
+    
+    obs, info = env.reset()  # Reset środowiska
+    print("Początkowa obserwacja:\n", obs)
+    print("Informacje:\n", info)
+
+    for i in range(1000):
+        action = env.action_space.sample()  # Losowa akcja
+        obs, reward, done, _, info = env.step(action)  # Wykonanie akcji
+
+    print("\nObserwacja po wykonaniu akcji:\n", obs)
+    print("Nagroda:", reward)
+    print("Czy koniec gry:", done)
+    print("Dodatkowe info:", info)
