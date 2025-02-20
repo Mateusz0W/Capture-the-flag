1512af0a91984d392e5f682f9b2935383ccf31be
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-02-20 17:05:58 +0100

    add ML configuration

--- config.py
+++ config.py
@@ -29,3 +29,16 @@ class WindowConfig:
 @dataclass(frozen=True)
 class GameConfig:
     players_in_team=1
+
+@dataclass(frozen=True)
+class MLConfig:
+    GAMMA=0.99
+    BATCH_SIZE=32
+    REPLAY_SIZE=10_000
+    REPLAY_START_SIZE=10_000
+    LEARNING_RATE= 1e-4
+    SYNC_TARGET_FRAMES = 1000
+    EPSILON_DECAY_LAST_FRAME = 150000
+    EPSILON_START = 1.0
+    EPSILON_FINAL = 0.01
+    MEAN_REWARD_BOUND = 50
