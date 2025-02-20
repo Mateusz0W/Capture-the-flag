5375e9755356758ed6257eed43fd6d1a21196154
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-02-20 17:13:14 +0100

    add max_episodes_steps

--- gym_environment/__init__.py
+++ gym_environment/__init__.py
@@ -3,4 +3,5 @@ from gym.envs.registration import register
 register(
     id='gym_environment/Capture-the-flag-v0',
     entry_point='gym_environment.environment:Environment',
+    max_episode_steps=500,
 )
