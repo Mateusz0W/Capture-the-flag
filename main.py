30c9630fd5d01b0d033fe8ef9d3a8647ba114414
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-02-20 17:05:06 +0100

    testing DQN models

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
+import torch
 import gym
 import gym_environment
+import numpy as np
+from machine_learning.dqn_model import DQN
 
 if __name__=="__main__":
     env=gym.make('gym_environment/Capture-the-flag-v0',render_mode='human')
+    state,_=env.reset()
     
-    obs, info = env.reset()  # Reset środowiska
-    print("Początkowa obserwacja:\n", obs)
-    print("Informacje:\n", info)
+    device = torch.device("cpu")
+    net =DQN(env.observation_space.shape[0],env.action_space.n).to(device)
+    net.load_state_dict(torch.load("machine_learning/models/Capture-the-flag-best_-102.dat",map_location=device))
+    print('Model loaded')
+    net.eval()
 
-    for i in range(1000):
-        action = env.action_space.sample()  # Losowa akcja
-        obs, reward, done, _, info = env.step(action)  # Wykonanie akcji
+    total_reward = 0
+    done = False
+    while True:
+        state_a = np.array([state], copy=False)
+        state_v = torch.tensor(state_a,dtype=torch.float32).to(device)
+        q_vals_v = net(state_v)
+        _, act_v = torch.max(q_vals_v, dim=1)
+        action = int(act_v.item())
+        
 
-    print("\nObserwacja po wykonaniu akcji:\n", obs)
-    print("Nagroda:", reward)
-    print("Czy koniec gry:", done)
-    print("Dodatkowe info:", info)
+        new_state, reward, done, _, _ = env.step(action)
+        total_reward += reward
+
+        if done:
+            state,_=env.reset()
+        else:
+
+            state = new_state
+        print(total_reward)
+    
+    
