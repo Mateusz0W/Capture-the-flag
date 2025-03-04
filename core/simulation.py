d6fc09dc5e89a2969c47ecf6ad16e45cc7b019a4
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-03-04 19:12:37 +0100

    change next_step and calculating reawrd for every player

--- core/simulation.py
+++ core/simulation.py
@@ -25,19 +25,18 @@ class Simulation:
             "Red": False,
         }
 
-    def next_step(self,action,direction,team):
-        #actions=['move','build']
-        #weights=[.9,.1]
-        #direction=['up','down','left','right','None']
-        for player in self.players:
-        #   action=random.choices(actions,weights,k=1)[0]
-            if player.team == team:
-                if action == 'build':
-                    player.build(direction,self.map)
-                else:
-                    player.move(direction,self.map)
+        self.rewards=np.zeros(GameConfig.players_in_team)
+
+    def next_step(self,action,direction,team,idx):
+        if team == 'Red':
+            idx=GameConfig.players_in_team + idx
+        
+        if action == 'build':
+            self.players[idx].build(direction,self.map)
+        else:
+            self.players[idx].move(direction,self.map)
             
-            player.captured_the_flag(self.flags)
+        self.players[idx].captured_the_flag(self.flags)
 
     def __end_game(self):
         for flag in self.flags:
@@ -45,8 +44,8 @@ class Simulation:
                 self.__add_point(flag)
                 self.game_over=True
 
-    def run(self,action,direction,team):
-        self.next_step(action,direction,team)
+    def run(self,action,direction,team,player_idx):
+        self.next_step(action,direction,team,player_idx)
         self.map.update(self.players,self.flags)
         self.__end_game()
         
@@ -65,43 +64,46 @@ class Simulation:
         #reawrds
         for team in self.team_captured_flag:
             self.team_captured_flag[team] = False
+        
+        self.rewards=np.zeros(GameConfig.players_in_team)
 
-
-    def reward(self,team):
+    def reward(self,team,idx):
+        if team == 'Red':
+            idx=GameConfig.players_in_team + idx
         reward=0
         distance_to_flag = lambda player,flag: abs(flag.x-player.x)+abs(flag.y-player.y)
         flags={
             "Red": self.flags[0],
             "Blue": self.flags[1],
         }
-        for player in self.players:
-            if player.has_a_flag and player.team == team and not self.team_captured_flag[team]:
-                reward += 30
-                self.team_captured_flag[team]=True
-                player.last_distances_to_flag.clear()
+        
+        if self.players[idx].has_a_flag and self.players[idx].team == team and not self.team_captured_flag[team]:
+            reward += 30
+            self.team_captured_flag[team]=True
+            self.players[idx].last_distances_to_flag.clear()
 
-            if not player.has_a_flag and player.team == team:
-                dist = distance_to_flag(player,flags[team])
-                if len(player.last_distances_to_flag) == player.last_distances_to_flag.maxlen:
-                    if dist < player.last_distances_to_flag[-1]:
-                        r = max(0.3,2-0.1*dist) if dist != 0 else 2
-                    else:
-                        r = -2
-                    reward += r
-                player.last_distances_to_flag.append(dist)
+        if not self.team_captured_flag[team] and self.players[idx].team == team:
+            dist = distance_to_flag(self.players[idx],flags[team])
+            if len(self.players[idx].last_distances_to_flag) == self.players[idx].last_distances_to_flag.maxlen:
+                if dist < self.players[idx].last_distances_to_flag[-1]:
+                    r = max(0.3,2-0.1*dist) if dist != 0 else 2
+                else:
+                    r = -2
+                reward += r
+            self.players[idx].last_distances_to_flag.append(dist)
                 
-            elif player.has_a_flag and player.team == team:
-                dist = distance_to_flag(player,flags["Blue"])
-                if len(player.last_distances_to_flag) == player.last_distances_to_flag.maxlen:
-                    if dist < player.last_distances_to_flag[-1]:
-                        r = max(0.2,3-0.2*dist)+1 if dist != 0 else 2
-                    else:
-                        r = -2
-                    reward += r
-                player.last_distances_to_flag.append(dist)
+        elif self.team_captured_flag[team] and self.players[idx].team == team:
+            dist = distance_to_flag(self.players[idx],flags["Blue"])
+            if len(self.players[idx].last_distances_to_flag) == self.players[idx].last_distances_to_flag.maxlen:
+                if dist < self.players[idx].last_distances_to_flag[-1]:
+                    r = max(0.2,3-0.2*dist)+1 if dist != 0 else 2
+                else:
+                    r = -2
+                reward += r
+            self.players[idx].last_distances_to_flag.append(dist)
 
-            if len(set(player.last_distances_to_flag)) <= 2 and player.team==team:
-                reward -=3
+        if len(set(self.players[idx].last_distances_to_flag)) <= 2 and self.players[idx].team==team:
+            reward -=3
 
         #reward for deliver flag to the base
         for flag in self.flags:
@@ -110,5 +112,6 @@ class Simulation:
             elif flag.in_opponent_base() and flag.team != team:
                 reward += 40
 
-        return reward
+        self.rewards[idx-4]=reward
+        return float(sum(self.rewards))
                 
